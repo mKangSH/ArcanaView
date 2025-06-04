83c93562bacf78554cd394f112e2f3e21f369eda
shkang2 <shkang2@piegroup.co.kr>
2025-06-04 18:07:15 +0900

    Zoom In/out 기능 구현준비 중

--- ArcanaView/View/ImageView.cpp
+++ ArcanaView/View/ImageView.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "ImageView.h"
+
+// static member
 uint64 ImageView::ImageViewId = 1;
 
 ImageView::ImageView(const std::wstring& imageFile) 
@@ -31,60 +33,56 @@ void ImageView::Render()
 
 void ImageView::Draw()
 {
-	ImGui::Begin(_title.c_str(), &_isVisible);
+	ImVec2 contentRegionSize = ImGui::GetWindowSize();
 
-	if (_contentRegionSize.x <= 0.0f || _contentRegionSize.y <= 0.0f)
+	ImVec2 mousePos = ImGui::GetMousePos();
+	if (ImGui::IsMouseDown(ImGuiMouseButton_Left))
+		//&& (mousePos.x >= screenPos.x && mousePos.x < (screenPos.x + contentRegionSize.x)) 
+		//&& (mousePos.y >= screenPos.y && mousePos.y < (screenPos.y + contentRegionSize.y)))
+	{
+		_windowFlags |= ImGuiWindowFlags_NoMove;
+	}
+	else if (ImGui::IsMouseReleasedWithDelay(ImGuiMouseButton_Left, 0.5))
+	{
+		_windowFlags &= ~ImGuiWindowFlags_NoMove;
+	}
+
+	ImGui::Begin(_title.c_str(), &_isVisible, _windowFlags);
+	
+	if (ImGui::IsWindowHovered())
+	{
+		_mousePos.x = std::clamp(_mousePos.x, 0.0f, _contentRegionSize.x - 1);
+		_mousePos.y = std::clamp(_mousePos.y, 0.0f, _contentRegionSize.y - 1);
+
+		uint8* pixels = _image.GetPixels();
+
+		float xRatio = _metadata.width / _contentRegionSize.x;
+		float yRatio = _metadata.height / _contentRegionSize.y;
+
+		_mousePos.x = std::clamp(ceil(_mousePos.x * xRatio), 0.0f, (float)_metadata.width - 1);
+		_mousePos.y = std::clamp(ceil(_mousePos.y * yRatio), 0.0f, (float)_metadata.height - 1);
+
+		int pixelIndex = ((_mousePos.y * _metadata.width) + _mousePos.x) * 4;
+
+		// pixes => B G R A
+		if (_metadata.format == DXGI_FORMAT_R8G8B8A8_UNORM)
+		{
+			ImGui::Text("X: %f Y: %f Value: %u %u %u %u", _mousePos.x, _mousePos.y, pixels[pixelIndex], pixels[pixelIndex + 1], pixels[pixelIndex + 2], pixels[pixelIndex + 3]);
+		}
+		else if (_metadata.format == DXGI_FORMAT_B8G8R8A8_UNORM)
+		{
+			ImGui::Text("X: %f Y: %f Value: %u %u %u %u", _mousePos.x, _mousePos.y, pixels[pixelIndex + 2], pixels[pixelIndex + 1], pixels[pixelIndex], pixels[pixelIndex + 3]);
+		}
+	}
+	else
 	{
 		_mousePos.x = 0;
 		_mousePos.y = 0;
 
 		ImGui::Text("X: %f Y: %f Value: %u %u %u %u", _mousePos.x, _mousePos.y, 0, 0, 0, 0);
 	}
-	else
-	{
-		if (ImGui::IsWindowHovered())
-		{
-			_mousePos.x = std::clamp(_mousePos.x, 0.0f, _contentRegionSize.x - 1);
-			_mousePos.y = std::clamp(_mousePos.y, 0.0f, _contentRegionSize.y - 1);
 
-			uint8* pixels = _image.GetPixels();
-
-			float xRatio = _metadata.width / _contentRegionSize.x;
-			float yRatio = _metadata.height / _contentRegionSize.y;
-
-			_mousePos.x = std::clamp(ceil(_mousePos.x * xRatio), 0.0f, (float)_metadata.width - 1);
-			_mousePos.y = std::clamp(ceil(_mousePos.y * yRatio), 0.0f, (float)_metadata.height - 1);
-
-			int pixelIndex = ((_mousePos.y * _metadata.width) + _mousePos.x) * 4;
-
-			ImGui::Text("X: %f Y: %f Value: %u %u %u %u", _mousePos.x, _mousePos.y, pixels[pixelIndex], pixels[pixelIndex + 1], pixels[pixelIndex + 2], pixels[pixelIndex + 3]);
-		}
-		else
-		{
-			_mousePos.x = 0;
-			_mousePos.y = 0;
-
-			ImGui::Text("X: %f Y: %f Value: %u %u %u %u", _mousePos.x, _mousePos.y, 0, 0, 0, 0);
-		}
-	}
-
-	ImDrawList* drawList = ImGui::GetWindowDrawList();
-	{
-		ImVec2 p0 = ImGui::GetCursorScreenPos();
-
-		_contentRegionSize = ImGui::GetContentRegionAvail();
-		
-		ImVec2 p1;
-		p1.x = p0.x + _contentRegionSize.x;
-		p1.y = p0.y + _contentRegionSize.y;
-
-		drawList->AddImage(reinterpret_cast<ImTextureID>(_shaderResourceView.Get()), p0, p1);
-
-		_mousePos = ImGui::GetMousePos();
-
-		_mousePos.x -= p0.x;
-		_mousePos.y -= p0.y;
-	}
+	UpdateDrawList();
 
 	ImGui::End();
 }
@@ -109,4 +107,44 @@ void ImageView::CreateImageTexture()
 		// TODO : Error Logging
 		return;
 	}
+}
+
+void ImageView::UpdateDrawList()
+{
+	ImDrawList* drawList = ImGui::GetWindowDrawList();
+
+	ImVec2 p0 = ImGui::GetCursorScreenPos();
+	_contentRegionSize = ImGui::GetContentRegionAvail();
+
+	ImVec2 p1 = _contentRegionSize;
+	p1.x += p0.x;
+	p1.y += p0.y;
+
+	drawList->AddImage(reinterpret_cast<ImTextureID>(_shaderResourceView.Get()), p0, p1);
+
+	ImVec2 mousePos = ImGui::GetMousePos();
+	if (ImGui::IsMouseDown(ImGuiMouseButton_Left) && 
+		(mousePos.x >= p0.x && mousePos.x < p1.x) &&
+		(mousePos.y >= p0.y && mousePos.y < p1.y))
+	{
+		if (_isLineTool == false)
+		{
+			_isLineTool = true;
+			_lastMousePos = ImGui::GetMousePos();
+		}
+		else 
+		{
+			ImVec2 position = ImGui::GetMousePos();
+			drawList->AddLine(_lastMousePos, position, ImU32(0xFFFFFFFF));
+		}
+	}
+	else if (ImGui::IsMouseReleased(ImGuiMouseButton_Left))
+	{
+		_isLineTool = false;
+	}
+
+	_mousePos = ImGui::GetMousePos();
+
+	_mousePos.x -= p0.x;
+	_mousePos.y -= p0.y;
 }
--- ArcanaView/View/ImageView.h
+++ ArcanaView/View/ImageView.h
@@ -18,6 +18,8 @@ private:
 	void Cleanup();
 	void CreateImageTexture();
 
+	void UpdateDrawList();
+
 private:
 	std::wstring _imageFile = L""; // Path to the image file
 
@@ -28,6 +30,11 @@ private:
 private:
 	ImVec2 _mousePos = { 0.0f, 0.0f };
 	ImVec2 _contentRegionSize = { 0.0f, 0.0f };
+
+private:
+	ImVec2 _lastMousePos = { 0.0f, 0.0f };
+	bool _isLineTool = false;
+	ImGuiWindowFlags _windowFlags = ImGuiWindowFlags_None;;
 
 public:
 	static uint64 ImageViewId;
